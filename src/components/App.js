7b7a24e0e706d413112d181232820a8198d40be8
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-28 15:39:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,50 @@ const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
 
-  return (
-    <div className="keyboard">
-      <div className="preview"></div>
-      <div>
-        {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
-            {key === " " ? "Space" : key.toUpperCase()}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+const [previewText, setPreviewText] = useState("");
+const [quote, setQuote] = useState("");
+
+useEffect(() => {
+if (previewText === "fourty two") {
+fetchQuote();
+} else {
+setQuote("");
+}
+}, [previewText]);
+
+const handleKeyPress = (key) => {
+const newPreviewText = previewText + key;
+setPreviewText(newPreviewText);
+};
+
+const fetchQuote = () => {
+fetch("https://api.quotable.io/random")
+.then((response) => response.json())
+.then((data) => {
+setQuote(data.content);
+})
+.catch((error) => {
+console.error("Error fetching quote:", error);
+});
+};
+
+return (
+<div className="keyboard">
+<div className="preview">{previewText}</div>
+{quote && <div className="quote">{quote}</div>}
+<div>
+{keys.map((key) => (
+<button
+key={key}
+id={key === " " ? "key-space" : key-${key}}
+onClick={() => handleKeyPress(key)}
+>
+{key === " " ? "Space" : key.toUpperCase()}
+</button>
+))}
+</div>
+</div>
+);
 };
 
 export default App;
