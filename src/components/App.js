47f8e26a7d820fef511e870c96c382282da20df8
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-28 15:59:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,51 +4,44 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-
-const [previewText, setPreviewText] = useState("");
-const [quote, setQuote] = useState("");
-
-useEffect(() => {
-if (previewText === "fourty two") {
-fetchQuote();
-} else {
-setQuote("");
-}
-}, [previewText]);
-
-const handleKeyPress = (key) => {
-const newPreviewText = previewText + key;
-setPreviewText(newPreviewText);
-};
-
-const fetchQuote = () => {
-fetch("https://api.quotable.io/random")
-.then((response) => response.json())
-.then((data) => {
-setQuote(data.content);
-})
-.catch((error) => {
-console.error("Error fetching quote:", error);
-});
-};
-
-return (
-<div className="keyboard">
-<div className="preview">{previewText}</div>
-{quote && <div className="quote">{quote}</div>}
-<div>
-{keys.map((key) => (
-<button
-key={key}
-id={key === " " ? "key-space" : key-${key}}
-onClick={() => handleKeyPress(key)}
->
-{key === " " ? "Space" : key.toUpperCase()}
-</button>
-))}
-</div>
-</div>
-);
+    const [input, setInput] = useState("");
+    const [quote, setQuote] = useState(null);
+    function handleClick(key) {
+        console.log("Clicked!");
+        setInput((state) => state + key);
+    }
+    useEffect(() => {
+        if (input === "forty two") {
+            (async () => {
+                const query = await fetch("https://api.quotable.io/random");
+                const response = await query.json();
+                setQuote(response.content);
+            })();
+        }
+    }, [input]);
+    return (
+        <>
+            {!quote ? (
+                <div className="keyboard">
+                    <div className="preview">{input}</div>
+                    <div>
+                        {keys.map((key) => (
+                            <button
+                                key={key}
+                                value={input}
+                                onClick={() => handleClick(key)}
+                                id={key === " " ? `key-space` : `key-${key}`}
+                            >
+                                {key === " " ? "Space" : key.toUpperCase()}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                <div className="quote">{quote}</div>
+            )}
+        </>
+    );
 };
 
 export default App;
